d97f700e76b5403fed19fbda00039b59fe133473
Hammas444 <hammassheikh17@gmail.com>
2026-03-03 00:56:49 +0500

    "Docker file added"

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,19 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5066";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 // Add services to the container.
 builder.Services.AddOpenApi();
 
 builder.Services.AddControllers();
 builder.Services.AddCors(options => {
-    options.AddPolicy("AllowNextJS",
-        policy => policy.WithOrigins("http://localhost:3000")
+    options.AddPolicy("AllowVercel",
+        policy => policy.WithOrigins("https://queue-simulator-app-tu4s.vercel.app/")
                         .AllowAnyMethod()
                         .AllowAnyHeader());
 });
 
 
 var app = builder.Build();
-app.UseCors("AllowNextJS");
+app.UseCors("AllowVercel");
 app.MapControllers();
 
 app.Run();
